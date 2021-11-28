5c8a36975973bbba39cc4b950348192d465500e1
zoemiya <zoemiya@orange.fr>
2021-11-28 12:31:25 +0100

    phase 8 : ORM(affichage en colonne des informations provenant de la BDD

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -47,4 +47,17 @@ class VisiteRepository extends ServiceEntityRepository
         ;
     }
     */
+    
+    /**
+     * Retourne toutes les visites triees sur un champ
+     * @param type $champ
+     * @param type $ordre
+     * @return Visite[]
+     */
+    public function findAllOrderBy($champ,$ordre):array{
+        return $this->createQueryBuilder('v')
+                ->orderBy('v.'.$champ, $ordre)
+                ->getQuery()
+                ->getResult();
+    }
 }
