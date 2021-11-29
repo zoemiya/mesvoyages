faf56ff7102b0d623796d316c61cab6990bc8ce9
zoemiya <zoemiya@orange.fr>
2021-11-29 20:05:12 +0100

    Phase 12 : ORM(suppression d'un enregistrement)

---
+++ src/Controller/admin/AdminVoyagesController.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller\admin;
+
+use App\Entity\Visite;
+use App\Repository\VisiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AdminVoyagesController
+ *
+ * @author lenovo
+ */
+class AdminVoyagesController extends AbstractController{
+    
+    
+    /**
+     * @var VisiteRepository
+    */
+    private $repository;
+    
+    /**
+     *
+     * @var EntityManagerInterface
+     */
+    private $om;
+    
+    /**
+     * @param VisiteRepository $repository
+     * @param EntityManagerInterface $om
+     */
+    function __construct(VisiteRepository $repository, EntityManagerInterface $om) {
+        $this->repository = $repository;
+        $this->om = $om;
+    }
+        
+  
+
+
+    
+    /**
+     * @Route("/admin", name="admin.voyages")
+     * @return Response
+     */  
+    public function index(): Response{
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this->render("admin.voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    /**
+     * @Route("/admin/suppr/{id}", name="admin.voyage.suppr")
+     * @param Visite $visite
+     * @return Response
+     */
+    public function suppr(Visite $visite): Response{
+        $this->om->remove($visite);
+        $this->om->flush();
+        return $this->redirectToRoute('admin.voyages');
+    }
+}
